286eb84bb20ee6a4b88ca6d9094261d0876abc19
Valentine-mario <valentine13400@gmail.com>
2018-05-16 16:09:11 +0100

    Update html.js

--- html.js
+++ html.js
@@ -1,8 +1,8 @@
 var color="#AACCFF"
 function hexdem(color){
-    var pattern1= new RegExp('[#][a-fA-F0-9]{3,8}', 'g');
-var newstring2=pattern1.test(color)
-console.log(newstring2)
+    var pattern1= new RegExp('[#][a-fA-F0-9]{3,8}', 'g');//matches for HTML hexadecimal colors
+var newstring2=pattern1.test(color)//check if the pattern exist 
+console.log(newstring2)//return true if it exist and flase if it don't
 }
 hexdem(color)
 
@@ -12,18 +12,18 @@ let text = `During today's presentation "Valentine Oragbakosi" presented first f
   "Ehiogu Chika Josephine" and finally "Chidera Jennifer".
  "Ebuka " will be presenting tomorrow.`
 function names(text){
-    var pattern2= /["][a-zA-Z.]+\s?\w+\s?\w*["]/gi
- var newstring3=text.match(pattern2).toString();
- var newstring3=newstring3.replace("\"","")
- console.log([newstring3])
+    var pattern2= /["][a-zA-Z.]+\s?\w+\s?\w*["]/gi//check for the name match between the double quote
+ var newstring3=text.match(pattern2).toString();//serch for the pattern and convert the array to a string
+ var newstring3=newstring3.replace("\"","")//replaces double quotes with an empty string
+ console.log([newstring3])//convert to an array and print out.
 }
 names(text)
  
 var genesys="http://genesystechhub.com"
 function url(genesys){
-   var pattern3= new RegExp('(^(https?|ftps?)?://([a-zA-Z1-9-_.*]+\.[A-Za-z]+))', 'gi')
-var newstr =genesys.replace(pattern3, "visit <a href='$1'>$3</a>")
-console.log(newstr)
+   var pattern3= new RegExp('(^(https?|ftps?)?://(www)?([a-zA-Z1-9-_.*]+\.[A-Za-z]+))', 'gi')//searches for the url pattern
+var newstr =genesys.replace(pattern3, "visit <a href='$1'>$4</a>")//replaces the url with a href link
+console.log(newstr)//outputs the link
 }
 url(genesys)
 
